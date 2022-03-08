871ad026d7d2e8e906aaa51a108d5c842e02ce82
Chris Bell <chrisbeckertonbell@gmail.com>
2022-03-07 19:14:31 -0800

    validates annual revenue, presence of names, store name length

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Naruto", last_name: "Uzumaki", hourly_rate: 120)
+@store1.employees.create(first_name: "Sasuke", last_name: "Uchiha", hourly_rate: 80)
+@store2.employees.create(first_name: "Izuku", last_name: "Midoriya", hourly_rate: 150)
+@store2.employees.create(first_name: "Katsuki", last_name: "Bakugo", hourly_rate: 90)
+@store2.employees.create(first_name: "Gohan", last_name: "Son", hourly_rate: 20)
